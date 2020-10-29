da4ec90ef2be9b610e5fabf6f871347f279b27a3
nathan-tw <106306027@nccu.edu.tw>
2020-10-29 17:28:41 +0800

    adjust search prediction

--- backend/database/postgres.go
+++ backend/database/postgres.go
@@ -38,7 +38,7 @@ func StorePrediction(MD5 string, size float64, malicious float64 ) {
 	fmt.Println("Inserted 1 row of data")
 }
 
-func SearchPrediction(MD5 string) {
+func SearchStaticPrediction(MD5 string) *sql.Rows{
 	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", HOST, PORT, USER, PASSWORD, DATABASE)
 	db, err := sql.Open("postgres", connectionString)
 	checkError(err)
@@ -48,8 +48,11 @@ func SearchPrediction(MD5 string) {
 	checkError(err)
 	fmt.Println("Successfully created connection to database")
 	
-	sql_statement := "SELECT * FROM dynamic_prediction WHERE (md5) VALUES ($1);"
-	_, err = db.Exec(sql_statement, MD5)
+	sql_statement := "SELECT * FROM static_prediction WHERE (md5) VALUES ($1);"
+	rows, err := db.Query(sql_statement, MD5)
 	checkError(err)
 	fmt.Println("Select 1 row of data")
+	err = rows.Err()
+	checkError(err)
+	return rows
 }
