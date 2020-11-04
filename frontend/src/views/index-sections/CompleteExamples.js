32c49c938983170c0b00746e3d822a4a7e70ada1
nathan-tw <106306027@nccu.edu.tw>
2020-11-04 18:42:22 +0800

    embed youtube introduction video

--- frontend/src/views/index-sections/CompleteExamples.js
+++ frontend/src/views/index-sections/CompleteExamples.js
@@ -2,6 +2,7 @@ import React from "react";
 
 // reactstrap components
 import { Container, Row, Col } from "reactstrap";
+import Youtube from "react-youtube";
 
 // core components
 
@@ -18,6 +19,7 @@ function CompleteExamples() {
                 本系統採用惡意程式動、靜態分析與機器學習技術，透過分析檔案內容與側錄其在虛擬環境中之實際表現作為檔案特徵進行分析，對未知程式進行偵測及分類，並產生詳細資訊報表，
                 供資安專業人員做進一步的追蹤與研究。
               </h4>
+              <Youtube videoId="UUuAbc-gFPk"></Youtube>
               <h4>
                 模型將根據受測檔案的增加定期進行校準、修正，以期能維持偵測準確率。本系統旨在幫助使用者更快速的檢測Windows的可執行檔，提供可靠的檢測評斷。
               </h4>
